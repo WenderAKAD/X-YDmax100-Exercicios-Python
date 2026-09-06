fd69892e46883ecad636d2e1f1b65f9e4aec78d9
WenderAKAD <wenndder.sp64@gmail.com>
2026-09-06 17:13:59 -0300

    17:53max100

---
+++ 17:35max100/Exercicio17max100.py
@@ -0,0 +1,10 @@
+'''Exercício 17: Par ou ímpar. - Leia um número inteiro e informe se ele é par ou ímpar. 
+'''
+num = int(input(' Digite um número inteiro: '))
+
+if num == 0:
+    print(' O não é um número válido. ')
+elif num % 2 == 0:
+    print(' {0} é um número par'.format(num))
+elif num % 2 == 1:
+    print(' {0} é um número ímpar. '.format(num))
