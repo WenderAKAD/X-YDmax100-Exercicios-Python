4fe8f7e98c72a613d6b8da63338697ed9e6a370c
Wender Araújo Santos <wenndder.sp64@gmail.com>
2026-09-15 19:34:53 -0300

    Definir se um número é positivo, negativo ou é zero.
    # Estruturas Condicionais: 
    '''Exercício 16: Positivo, Negativo ou Zero. Leia um número real e informe se é positivo, negativo ou se o número é igual a zero. 
    '''

--- 16:35max100/Estruturas_condicionais.py
+++ 16:35max100/Estruturas_condicionais.py
@@ -1,7 +1,16 @@
 # Estruturas Condicionais: 
 '''Exercício 16: Positivo, Negativo ou Zero. Leia um número real e informe se é positivo, negativo ou se o número é igual a zero. 
 '''
-num = int(input(' Digite um número inteiro: '))
+
+def traco40():
+    print('-'*40,)
+
+traco40()
+print(' Vamos descorbrir se o número é positivo, negativo ou Zero. ')
+traco40()
+
+
+num = int(input('\n Digite um número inteiro: '))
 if num < 0:
     print('{0} é negativo.'.format(num))
 elif num == 0:
